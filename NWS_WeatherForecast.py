414ee2fa9ffbde3525809e351acdbac63125f630
sadiecee <79473322+sadiecee@users.noreply.github.com>
2021-04-08 20:57:16 -0700

    Update NWS_WeatherForecast.py
    complete commented code

--- NWS_WeatherForecast.py
+++ NWS_WeatherForecast.py
@@ -1,5 +1,9 @@
 '''
-Don't forget the comment block!
+Sadie Murray
+Python 3 
+4/8/2021
+This is a webscrapping code that takes a lat/long value input by the user and outputs a five day weather forecast for that lat/long value 
+from forecast.weather.gov. 
 '''
 
 # import required libraries
@@ -11,13 +15,22 @@ forecast = []
 
 ## Provide the latitude and longitude for the location you would like to check the forecast for
 ## Lat/lon in decimal degrees provided for Worcester, MA
-lat = '42.2634'
-lon = '-71.8022'
+lat = str(round(float(input("Please input your latitude in decimal degrees: ")),4))
+lon = str(round(float(input("Please input your longitude in decimal degrees: ")),4))
+#lat = '42.2634'
+#lon = '-71.8022'
 
 # Create url for the requested location through string concatenation
 url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
+
 # Check if the URL exists
 # print url
+request = requests.get(url)
+if request.status_code == 200:
+    print(url)
+else: 
+  print("Error. No data for that location.")
+
 
 # Send request to retrieve the web-page using the get() function from the requests library
 # The page variable stores the response from the web-page
@@ -41,4 +54,8 @@ for i in weather_forecast:
 
 # Print list to remove unicode characters
 for day in forecast:
-    print(day)
+    day = day.replace('A',' A')
+    day = day.replace('L',', L')
+    day = day.replace('H',', H')
+    day = day.replace('N',' N')
+    print(day.upper())
